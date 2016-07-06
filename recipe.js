e8bdca47a00d4c973699863c272a11047278afda
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-06 16:44:37 -0400

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -4,3 +4,7 @@ function addIngredient(recipe, ingredient, amounts){
 	recipe[ingredient] = amounts;
 	return recipe;
 } 
+function removeIngredient(recipe, ingredient) {
+	delete recipe[ingredient];
+	return recipe;
+}
