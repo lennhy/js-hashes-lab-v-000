c3f18fb652ff47c227ef793e9f5985504d70642c
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-07 05:02:18 +0000

    done

--- recipe.js
+++ recipe.js
@@ -1,14 +1,21 @@
 'use strict';
 
 function addIngredient(recipe, ingredient, amounts){
-	recipe[ingredient] = amounts;
+	recipe[ingredient] = amounts; //add value to key
 	return recipe;
-} 
+}
 function removeIngredient(recipe, ingredient) {
-	delete recipe[ingredient];
+	delete recipe[ingredient]; //remove value
 	return recipe;
 }
 function updateIngredient(recipe, ingredient, amounts) {
-  
-	return addIngredient(recipe, ingredient, amounts);
+
+	return addIngredient(recipe, ingredient, amounts); //helper function -same logic as addIngredient
+}
+function readRecipe(recipe) {
+	var amount; //create a local variable
+	for (var ingredient in recipe){ //key in object
+		console.log(ingredient);
+	 return	`this recipe calls for ${recipe[ingredient]}  of ${ingredient}`; //return value and key in statement
+ }
 }
